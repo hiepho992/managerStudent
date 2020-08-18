30fafe53ea5ae2443149e4b16e9b853f813c28f2
hiepho992 <dinhhiep211992@gmail.com>
2020-08-18 15:40:16 +0700

    update

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -15,7 +15,7 @@ class UserSeeder extends Seeder
         $user = new User();
         $user->name = "Hồ Đình Hiệp";
         $user->email = "dinhhiep@gmail.com";
-        $user->password ="12345678";
+        $user->password = bcrypt('12345678');
         $user->save();
     }
 }
